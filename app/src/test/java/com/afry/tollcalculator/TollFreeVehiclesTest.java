f2876d96e52643ba60f75ddeae5942c16a4689ab
Nils-Gunnar Nilsson <ngn925@gmail.com>
2024-11-14 17:14:27 +0100

    Add test of isTollFree in enum

--- app/src/test/java/com/afry/tollcalculator/TollFreeVehiclesTest.java
+++ app/src/test/java/com/afry/tollcalculator/TollFreeVehiclesTest.java
@@ -1,11 +1,11 @@
 package com.afry.tollcalculator;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,5 +32,27 @@ class TollFreeVehiclesTest {
     @MethodSource("provideStringsForFromString")
     void fromStringTest(String vehicle, Optional<TollFreeVehicles> expected) {
         assertEquals(expected, TollFreeVehicles.fromString(vehicle));
+    }
+
+    private static Stream<Arguments> provideDataForIsTollFree() {
+        return Stream.of(
+                Arguments.of("Car", Optional.of(TollFreeVehicles.CAR), false),
+                Arguments.of("car", Optional.of(TollFreeVehicles.CAR), false),
+                Arguments.of("Diplomat", Optional.of(TollFreeVehicles.DIPLOMAT), true),
+                Arguments.of("DIPLOMAT", Optional.of(TollFreeVehicles.DIPLOMAT), true),
+                Arguments.of("Foreign", Optional.of(TollFreeVehicles.FOREIGN), true),
+                Arguments.of("Emergency", Optional.of(TollFreeVehicles.EMERGENCY), true),
+                Arguments.of("Military", Optional.of(TollFreeVehicles.MILITARY), true),
+                Arguments.of("Motorbike", Optional.of(TollFreeVehicles.MOTORBIKE), true),
+                Arguments.of("Tractor", Optional.of(TollFreeVehicles.TRACTOR), true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDataForIsTollFree")
+    void isTollFreeTest(String vehicleId, Optional<TollFreeVehicles> vehicle, boolean expected) {
+        AtomicBoolean isTollFree = new AtomicBoolean(false);
+        vehicle.ifPresent(v -> isTollFree.set(v.isTollFree()));
+        assertEquals(expected, isTollFree.get());
     }
 }
